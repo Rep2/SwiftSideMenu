a0a343d3ad7edfc08d4c9f636dd3576fe563596f
Ivan Rep <ivan.rep2@gmail.com>
2017-12-04 20:25:34 +0100

    Initial commit

---
+++ SwiftSideMenu/SwiftSideMenu.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSideMenu.h
+//  SwiftSideMenu
+//
+//  Created by Ivan Rep on 04/12/2017.
+//  Copyright © 2017 Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSideMenu.
+FOUNDATION_EXPORT double SwiftSideMenuVersionNumber;
+
+//! Project version string for SwiftSideMenu.
+FOUNDATION_EXPORT const unsigned char SwiftSideMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSideMenu/PublicHeader.h>
+
+
