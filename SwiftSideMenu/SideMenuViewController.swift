9ea739801ea450a15ee863f2da671eaa7dbed5e3
Ivan Rep <ivan.rep2@gmail.com>
2017-12-05 03:44:39 +0100

    SideMenuViewController custom fix

--- SwiftSideMenu/SideMenuViewController.swift
+++ SwiftSideMenu/SideMenuViewController.swift
@@ -1,9 +1,19 @@
 import UIKit
 
 open class SideMenuViewController: UIViewController {
-    override open func viewDidLoad() {
-        super.viewDidLoad()
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
 
+        setupView()
+    }
+
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+        setupView()
+    }
+
+    func setupView() {
         modalPresentationStyle = .custom
     }
 }
